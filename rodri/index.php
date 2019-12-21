0e9980a98e4db6a00b6a152b7673b726d3e505b0
EnriqMil <49676174+EnriqMil@users.noreply.github.com>
2019-12-21 21:53:15 +0100

    Create index.php

---
+++ rodri/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<!--Se escribe sobre el controlador ya que incluye vista y modelo -->
+<head>
+    <meta charset="UTF-8">
+    <title>Documento sin titulo </title>
+<style>
+h1{text-align:center;
+
+}
+</style>
+</head>
+<body>
+<link type="text/css" rel="stylesheet" href="vista/css/styleportal.css">
+<h1>Modelo Vista controlador</h1>
+<br>
+<?php
+//punto de partida donde comienza todo 
+	include("vistas/vper.php"); 
+?>
+
+
+    
+</body>
+</html>
